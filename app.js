cdb7843b008ca838764ad10d2d64e2d126f0ca0e
patiskiba <patricia.skiba@mscoding.org>
2025-06-02 16:36:53 -0500

    Enemy sprite showing up

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const url = "https://pokeapi.co/api/v2/pokemon/";
 let pokemonName = "pikachu";
 const endpoint = url + pokemonName;
 
-
+let enemyPokemonSprite = document.getElementById("enemyPokemonSprite");
+console.log(enemyPokemonSprite);
 
 fetch(endpoint)
     .then(response => {
         
         return response.json();
     })
-    .then(data => console.log(data.sprites.front_default));
+    .then(data => {
+        let sprite = data.sprites.front_default;
+        console.log(data.sprites.front_default);
+        
+        enemyPokemonSprite.src = sprite;
+    });
 
 
 
