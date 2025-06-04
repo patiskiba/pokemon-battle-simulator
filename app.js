573415ce8c45d70b92a693502b6ec635c41a2d7a
patiskiba <patricia.skiba@mscoding.org>
2025-06-04 13:31:14 -0500

    async

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-const url = "https://pokeapi.co/api/v2/pokemon/";
-let pokemonName = "pikachu";
-const endpoint = url + pokemonName;
+const playerPokemonList = [];
+const enemyPokemonList = [];
+const MAX_POKEMON_PER_SIDE = 3;
+
+// const url = "https://pokeapi.co/api/v2/pokemon/";
+// let pokemonName = "pikachu";
+// const endpoint = url + pokemonName;
 
 let enemyPokemonSprite = document.getElementById("enemyPokemonSprite");
 console.log(enemyPokemonSprite);
 
-fetch(endpoint)
-    .then(response => {
-        
-        return response.json();
-    })
-    .then(data => {
+
+
+async function addPokemonToEnemyList() {
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+        const data = await response.json();
+        console.log(data);
         let sprite = data.sprites.front_default;
-        console.log(data.sprites.front_default);
-        
         enemyPokemonSprite.src = sprite;
-    });
+    } catch (error) {
+        console.error('Error fetching Pokemon:', error);
+    }
+}
+
+addPokemonToEnemyList()
+
+// fetch(endpoint)
+//     .then(response => {
+        
+//         return response.json();
+//     })
+//     .then(data => {
+//         let sprite = data.sprites.front_default;
+//         console.log(data.sprites.front_default);
+//         enemyPokemonSprite.src = sprite;
+//     });
 
 
 
