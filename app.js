7d979857dc5702a2adeee12a23d70b508c482c1d
patiskiba <patricia.skiba@mscoding.org>
2025-06-04 22:33:12 -0500

    Got 3 pokemon to show up

--- app.js
+++ app.js
@@ -2,24 +2,47 @@ const playerPokemonList = [];
 const enemyPokemonList = [];
 const MAX_POKEMON_PER_SIDE = 3;
 
-// const url = "https://pokeapi.co/api/v2/pokemon/";
-// let pokemonName = "pikachu";
-// const endpoint = url + pokemonName;
+const url = "https://pokeapi.co/api/v2/pokemon/";
 
 let enemyPokemonSprite = document.getElementById("enemyPokemonSprite");
 console.log(enemyPokemonSprite);
 
+class Pokemon {
+    constructor(name) {
+        this.name = name;
 
+    }
+}
+
+let pokemonName;
 
 async function addPokemonToEnemyList() {
-    try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+    for (let i = 1; i <= MAX_POKEMON_PER_SIDE; i++) {
+        
+        if (i == 1) {
+            pokemonName = "pikachu";
+        } else if (i == 2) {
+            pokemonName = "rayquaza";
+        } else if (i == 3) {
+            pokemonName = "milotic"
+        } 
+
+        let endpoint = url + pokemonName;
+
+        try {
+        const response = await fetch(endpoint);
         const data = await response.json();
         console.log(data);
         let sprite = data.sprites.front_default;
         enemyPokemonSprite.src = sprite;
-    } catch (error) {
+        
+        
+
+        
+
+        } catch (error) {
         console.error('Error fetching Pokemon:', error);
+        }
     }
 }
 
